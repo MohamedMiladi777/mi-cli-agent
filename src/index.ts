f3eff89baa2d421ebbb2305e7bcc9912e845c9ba
MohamedMiladi777 <miladyshady@gmail.com>
2026-04-15 15:51:19 +0100

    Drop files from .gitignore

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { render } from 'ink';
-import { App } from './ui/index.tsx';
-
-render(React.createElement(App));
--- src/utils/debugger.ts
+++
@@ -1,8 +0,0 @@
-import { appendFileSync } from "fs";
-import { join } from "path";
-
-const DEBUG_LOG = join(process.cwd(), "debug.log");
-
-export function debugLog(message: string) {
-  appendFileSync(DEBUG_LOG, `${new Date().toISOString()} - ${message}\n`);
-}
